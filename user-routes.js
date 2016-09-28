a8d54927d28d9dd49df115d538b008ff9500d1e9
Stephen Collins <stephen.collins@centeva.com>
2016-09-28 16:35:11 -0400

    added roles to token

--- user-routes.js
+++ user-routes.js
@@ -4,23 +4,26 @@ const express = require('express'),
 
 const app = module.exports = express.Router();
 
-function createToken(username) {
+function createToken(username, roles) {
   return jwt.sign({
     id: 1,
-    username: username
-  }, 
-  config.secret, { 
-    expiresIn: 60*60*5 
+    username: username,
+    roles: roles
+  },
+  config.secret, {
+    expiresIn: 60*60*5
   });
 }
 
 app.post('/sessions/create', function(req, res) {
 
-  if (!(req.body.username && req.body.password)) {
+  const username = req.body.username;
+
+  if (!(username && req.body.password)) {
     return res.status(401).json({ message: "You must provide a username and password" });
   }
 
   return res.status(201).json({
-    id_token: createToken(req.body.username)
+    id_token: createToken(username, username === "nibblesnbits" ? ["admin"] : [])
   });
 });
